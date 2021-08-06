b4908e76894d1063e231962b966d88bf5c9fe163
aliansi1 <anssw2003@outlook.com>
2021-08-06 19:01:51 +0300

    Update py.py

--- py.py
+++ py.py
@@ -4,13 +4,26 @@ import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 import streamlit as st 
 import requests as request
+import argparse
+import sys
+
+def parse_args(args):
+    parser = argparse.ArgumentParser('Data Diagnostics')
+    return parser.parse_args(args)
+
+
+@st.cache
+ 
+
+
 
 def do_something(sc): 
     #print("Doing stuff...")
     msgs = [{'topic': "gammvert/pithiviers/AAA", 'payload': "aaaaaaaa"}, ("gammvert/pithiviers/BBB", "bbbbbb", 0, False)]
     publish.multiple(msgs, hostname="broker.mqtt-dashboard.com")  
     # do your stuff
-    st.write( simplejson.loads(request.POST.get('data')) )
+    args = parse_args(sys.argv[1:])
+    st.write( args )
 
 client = mqtt.Client()
 client.connect("broker.mqtt-dashboard.com", 1883, 60)    
