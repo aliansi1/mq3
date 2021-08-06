5d4ffed6b8cd9846d01395982e6a88fd6ff16503
aliansi1 <anssw2003@outlook.com>
2021-08-06 20:07:33 +0300

    Update py.py

--- py.py
+++ py.py
@@ -4,9 +4,8 @@ import paho.mqtt.publish as publish
 import streamlit as st 
 from flask import Flask, request
 import json
-app = Flask(__name__)
+ 
 
-@app.route('/', methods=['GET', 'POST'])
 def refresh():
     st.write("okk")
     params = {
@@ -23,7 +22,7 @@ def refresh():
 
 client = mqtt.Client()
 client.connect("broker.mqtt-dashboard.com", 1883, 60)    
-app.run() 
+refresh()
 
 
  
