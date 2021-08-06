7ab22ebabb7e81165cd1b9540987e8f0ec16e0ff
aliansi1 <anssw2003@outlook.com>
2021-08-06 18:34:27 +0300

    Update py.py

--- py.py
+++ py.py
@@ -1,4 +1,19 @@
-print("This line will be printed.")
+import simplejson
+import paho.mqtt.client as mqtt
+#import context  # Ensures paho is in PYTHONPATH
+import paho.mqtt.publish as publish
+import streamlit as st 
+ 
+def do_something(sc): 
+    #print("Doing stuff...")
+    msgs = [{'topic': "gammvert/pithiviers/AAA", 'payload': "aaaaaaaa"}, ("gammvert/pithiviers/BBB", "bbbbbb", 0, False)]
+    publish.multiple(msgs, hostname="broker.mqtt-dashboard.com")  
+    # do your stuff
+    st.write( simplejson.loads(request.POST.get('data')) )
+
+client = mqtt.Client()
+client.connect("broker.mqtt-dashboard.com", 1883, 60)    
+ 
 
 
  
