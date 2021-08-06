9bd1f7e3203a0c517061f63dafd0746616e70292
aliansi1 <anssw2003@outlook.com>
2021-08-06 19:46:40 +0300

    Update py.py

--- py.py
+++ py.py
@@ -9,8 +9,8 @@ def do_something(sc):
     #print("Doing stuff...")
     msgs = [{'topic': "gammvert/pithiviers/AAA", 'payload': "aaaaaaaa"}, ("gammvert/pithiviers/BBB", "bbbbbb", 0, False)]
     publish.multiple(msgs, hostname="broker.mqtt-dashboard.com")  
-    if rq.method == 'POST':  #this block is only entered when the form is submitted
-     st.write(  rq.form.get('ee'))
+    #if rq.method == 'POST':  #this block is only entered when the form is submitted
+    st.write("xxx")
  
 
 client = mqtt.Client()
