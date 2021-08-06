d8c75294277179a86cb11612455bae4775080802
aliansi1 <anssw2003@outlook.com>
2021-08-06 19:56:37 +0300

    Update py.py

--- py.py
+++ py.py
@@ -2,26 +2,27 @@ import paho.mqtt.client as mqtt
 #import context  # Ensures paho is in PYTHONPATH
 import paho.mqtt.publish as publish
 import streamlit as st 
-from flask import request
+from flask import Flask, request
+import json
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def refresh():
+    params = {
+        'thing1': request.values.get('ee'),
+        'thing2': request.get_json().get('55')
+    }
+    st.write(params)
+    return json.dumps(params)
 
 
-@app.route("/lookupmember", methods=["POST"])
-def lookupmember():
-    member = request.data
-   st.write(member)
-    return jsonify(member.decode("utf-8"))
 
-def do_something(sc): 
-    #print("Doing stuff...")
-    msgs = [{'topic': "gammvert/pithiviers/AAA", 'payload': "aaaaaaaa"}, ("gammvert/pithiviers/BBB", "bbbbbb", 0, False)]
-    publish.multiple(msgs, hostname="broker.mqtt-dashboard.com")  
-    #if rq.method == 'POST':  #this block is only entered when the form is submitted
-    st.write("xxx")
+   
  
 
 client = mqtt.Client()
 client.connect("broker.mqtt-dashboard.com", 1883, 60)    
- 
+app.run() 
 
 
  
