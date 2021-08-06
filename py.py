33ddd0da687354de938692e2839b47363f11694f
aliansi1 <anssw2003@outlook.com>
2021-08-06 19:37:11 +0300

    Update py.py

--- py.py
+++ py.py
@@ -2,19 +2,14 @@ import paho.mqtt.client as mqtt
 #import context  # Ensures paho is in PYTHONPATH
 import paho.mqtt.publish as publish
 import streamlit as st 
-import sys
+import request as rq
 
 def do_something(sc): 
     #print("Doing stuff...")
     msgs = [{'topic': "gammvert/pithiviers/AAA", 'payload': "aaaaaaaa"}, ("gammvert/pithiviers/BBB", "bbbbbb", 0, False)]
     publish.multiple(msgs, hostname="broker.mqtt-dashboard.com")  
-    # do your stuff
-    
-@view_config(route_name='newusers', renderer='json')
-def newusers(self):
-    name =  str(self.request.POST.get('ee'))
-    st.write(name)
-    return name 
+    if rq.method == 'POST':  #this block is only entered when the form is submitted
+     st.write(  rq.form.get('ee'))
  
 
 client = mqtt.Client()
