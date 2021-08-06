3f87a8d164345e04f75234b1fe74913fc4452b65
aliansi1 <anssw2003@outlook.com>
2021-08-06 20:18:51 +0300

    Update py2.py

--- py2.py
+++ py2.py
@@ -1,7 +1,27 @@
-import streamlit as st
-from streamlit_ace import st_ace
+import paho.mqtt.client as mqtt
+#import context  # Ensures paho is in PYTHONPATH
+import paho.mqtt.publish as publish
+import streamlit as st 
+from flask import Flask, request
+import json
+ 
 
-st.write("hello")
+def refresh():
+    st.write("okk")
+    params = {
+        'thing1': request.values.get('ee'),
+        'thing2': request.get_json().get('55')
+    }
+    st.write(params)
+    return json.dumps(params)
 
+
+
+   
+ 
+
+client = mqtt.Client()
+client.connect("broker.mqtt-dashboard.com", 1883, 60)    
+refresh()
 
  
