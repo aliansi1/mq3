1f84f242fd23b8e44417203ce119bddec2c026c4
aliansi1 <anssw2003@outlook.com>
2021-09-10 17:10:20 +0300

    Update mqtt2.py

--- mqtt2.py
+++ mqtt2.py
@@ -7,16 +7,28 @@ import http.client
 
 s = sched.scheduler(time.time, time.sleep)
 def do_something(sc): 
-     conn = http.client.HTTPSConnection("firestore.googleapis.com")
-     payload = ''
-     headers = {}
-     conn.request("GET", "/v1/projects/inlaid-particle-296016/databases/(default)/documents/c/doc1?key=AIzaSyCCIHwUPFBFve6bjRrkclHmGpoNfsvy2sM", payload, headers)
-     res = conn.getresponse()
-     data = res.read()
+   #  conn = http.client.HTTPSConnection("firestore.googleapis.com")
+    # payload = ''
+    # headers = {}
+    # conn.request("GET", "/v1/projects/inlaid-particle-296016/databases/(default)/documents/c/doc1?key=AIzaSyCCIHwUPFBFve6bjRrkclHmGpoNfsvy2sM", payload, headers)
+    # res = conn.getresponse()
+    # data = res.read()
      #print(data.decode("utf-8"))
     #print("Doing stuff...")
     #msgs = [{'topic': "gammvert/pithiviers/AAA", 'payload': data.decode("utf-8")}, ("gammvert/pithiviers/BBB", "bbbbbbNEW HANID", 0, False)]
-     client.publish("send/to/LGLS7752a1d2e6", data.decode("utf-8"))  
+  conn = http.client.HTTPSConnection("mikmon2.000webhostapp.com")
+  payload = ''
+  headers = {}
+  conn.request("GET", "/LGLS7752a1d2e62.php", payload, headers)
+  res = conn.getresponse()
+  data = res.read()
+  #print(data.decode("utf-8"))
+  ini_string = "{
+  'LGLS7752a1d2e6':{ 
+  'active':'"+data.decode("utf-8")+"',  'brgtotal':'9999'
+  }
+  }"
+  client.publish("send/to/LGLS7752a1d2e6", ini_string)  
     # do your stuff
      s.enter(1, 1, do_something, (sc,))
 
