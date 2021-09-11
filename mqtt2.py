ce5ca5345e534e5a79c6eb1ab43f6b193197d930
aliansi1 <anssw2003@outlook.com>
2021-09-11 14:21:47 +0300

    Update mqtt2.py

--- mqtt2.py
+++ mqtt2.py
@@ -6,6 +6,7 @@ import requests
 
 s = sched.scheduler(time.time, time.sleep)
 def do_something(sc): 
+  try:
    #  conn = http.client.HTTPSConnection("firestore.googleapis.com")
     # payload = ''
     # headers = {}
@@ -16,15 +17,17 @@ def do_something(sc):
     #print("Doing stuff...")
     #msgs = [{'topic': "gammvert/pithiviers/AAA", 'payload': data.decode("utf-8")}, ("gammvert/pithiviers/BBB", "bbbbbbNEW HANID", 0, False)]
   
-  url = "https://mikmon2.000webhostapp.com/LGLS7752a1d2e62.php"
-  payload={}
-  headers = {}
-  response = requests.request("GET", url, headers=headers, data=payload)
-  #print(response.text)
-  ini_string = "{ 'LGLS7752a1d2e6':{  'active':'"+response.text+"',  'brgtotal':'9999'  }  }"
-  client.publish("send/to/LGLS7752a1d2e6palm", ini_string)  
-  #print(data.decode("utf-8"))
-  s.enter(1, 1, do_something, (sc,))
+   url = "https://mikmon2.000webhostapp.com/LGLS7752a1d2e62.php"
+   payload={}
+   headers = {}
+   response = requests.request("GET", url, headers=headers, data=payload)
+   #print(response.text)
+   ini_string = "{ 'LGLS7752a1d2e6':{  'active':'"+response.text+"',  'brgtotal':'9999'  }  }"
+   client.publish("send/to/LGLS7752a1d2e6palm", ini_string)  
+   #print(data.decode("utf-8"))
+   s.enter(1, 1, do_something, (sc,))
+  except:
+   x=1
 
 client = mqtt.Client()
 client.connect("broker.mqtt-dashboard.com", 1883, 60)    
