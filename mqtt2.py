b31e4bf7b16b34e1af2efa4ad6fa08bb7e4416b9
aliansi1 <anssw2003@outlook.com>
2021-09-23 04:47:27 +0300

    Update mqtt2.py

--- mqtt2.py
+++ mqtt2.py
@@ -35,7 +35,7 @@ def do_something(sc):
     #[{"hs":"2B22ECF8A532234696DB27E3768B72FA","topc":"FED472E99D2C9773501BB58CBF2FDD2BC3CF228100D494AB4A70C4367B808B28","str":";all;queue2;","active":"19","LAN":"0","WAN":"0","WAN1":"0","WAN2":"0","WAN3":"0","WAN4":"0","WAN5":"0","WAN6":"0","WAN7":"0","WAN8":"0","WAN9":"0","WAN10":"0","WAN11":"0","WAN12":"0","lines":"0","regidtred":"0","bound1":"0","bound2":"0","rate":"0","speedauto":"0"},{"hs":"2B22ECF8A532234696DB27E3768B72FA","topc":"2B22ECF8A532234696DB27E3768B72FA","str":"0","active":"19","LAN":"0","WAN":"0","WAN1":"0","WAN2":"0","WAN3":"0","WAN4":"0","WAN5":"0","WAN6":"0","WAN7":"0","WAN8":"0","WAN9":"0","WAN10":"0","WAN11":"0","WAN12":"0","lines":"0","regidtred":"0","bound1":"0","bound2":"0","rate":"0","speedauto":"0"}]
    #tpc =msgs[msgs.find("topic")+8,msgs.find("hs")-3]      
    ini_string = "{ 'LGLS7752a1d2e6':{  'active':'"+response.text+"',  'brgtotal':'9999'  }  }"
-   print(msgs[:-1]+"]")
+  #print(msgs[:-1]+"]")
    publish.multiple(entry1List, hostname="broker.mqtt-dashboard.com")  
    #client.publish("send/to/LGLS7752a1d2e6palm", ini_string)  
    #client.publish("send/to/FED472E99D2C9773501BB58CBF2FDD2BC3CF228100D494AB4A70C4367B808B28", ini_string)  
