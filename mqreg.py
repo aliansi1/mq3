023171b94eaadca99dffc5313e57119fc3f6b30f
aliansi1 <anssw2003@outlook.com>
2022-03-01 18:16:02 +0300

    Add files via upload

---
+++ mqreg.py
@@ -0,0 +1,40 @@
+import paho.mqtt.client as mqtt
+#import context  # Ensures paho is in PYTHONPATH
+import paho.mqtt.publish as publish
+import sched, time
+import requests
+import json 
+
+s = sched.scheduler(time.time, time.sleep)
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+ 
+  
+def do_something(): 
+  try:
+ 
+  
+   url = "https://mikmon2.000webhostapp.com/getregmikdata.php?hs="+""
+   payload={}
+   headers = {}
+   response = requests.request("GET", url, headers=headers, data=payload)
+ 
+   data = json.loads("{\"data\":"+response.text+"}")
+
+   entry1List = []
+   print(response.text)
+   for i in data['data']:
+      client.subscribe("shadmik/reg/mikmon/v1/"+str(i["dvc"]))
+      print("shadmik/reg/mikmon/v1/"+str(i["dvc"]))
+      entry1List.append({'topic': "shadmik/reg/mikmon/v1/"+str(i["dvc"]), 'payload':  str(i)})
+      
+  
+   publish.multiple(entry1List, hostname="broker.mqtt-dashboard.com")  
+  except:
+   x=1
+
+client = mqtt.Client()
+client.connect("broker.mqtt-dashboard.com", 1883, 30)    
+do_something()
+#s.enter(2, 1, do_something, (s,))
+#s.run()
