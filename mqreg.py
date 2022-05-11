e7b843d50ee563cb9cffdbb31dc1c430850305b1
aliansi1 <anssw2003@outlook.com>
2022-05-11 19:31:41 +0300

    Update mqreg.py

--- mqreg.py
+++ mqreg.py
@@ -14,7 +14,7 @@ def do_something():
   #try:
  
   
-   url = "https://mikmon2.000webhostapp.com/getregmikdata.php?hs=7126B184305BDBFFA0612C3CAF30B50C4C474C5337373532613164326536"
+   url = "https://mikmon2.000webhostapp.com/getregmikdata.php?hs=FCBA63725BE8BB0CF7AF93E328536DF1"
    payload={}
    headers = {}
    response = requests.request("GET", url, headers=headers, data=payload)
